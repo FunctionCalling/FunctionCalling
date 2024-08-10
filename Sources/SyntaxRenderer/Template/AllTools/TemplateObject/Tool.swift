9f5f4bef4e4607441b1307cdaeced1165bf79f7b
Fumito Ito <weathercook@gmail.com>
2024-08-11 00:21:54 +0900

    tool should have different keys for each services

--- Sources/SyntaxRenderer/Template/AllTools/TemplateObject/Tool.swift
+++ Sources/SyntaxRenderer/Template/AllTools/TemplateObject/Tool.swift
@@ -11,9 +11,11 @@ import CommonModules
 extension Tool {
     /// Initializes a `Tool` instance from a `FunctionDeclaration`.
     /// - Parameter decl: The `FunctionDeclaration` to initialize from.
+    /// - Parameter service: The `FunctionCallingService` to use.
     /// - Throws: An error if the initialization fails.
-    init(from decl: FunctionDeclaration) throws {
+    init(from decl: FunctionDeclaration, service: FunctionCallingService) throws {
         self.init(
+            service: service,
             name: decl.name,
             description: decl.description,
             inputSchema: try InputSchema(from: decl)
